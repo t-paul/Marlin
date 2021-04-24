fce499fe6231122f5a49c2f591226529992087ff
Torsten Paul <Torsten.Paul@gmx.de>
2021-04-24 04:52:54 +0200

    Reduce homing feedrate.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1595,7 +1595,7 @@
 #endif
 
 // Homing speeds (mm/min)
-#define HOMING_FEEDRATE_MM_M { (50*60), (50*60), (4*60) }
+#define HOMING_FEEDRATE_MM_M { (40*60), (40*60), (2*60) }
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
