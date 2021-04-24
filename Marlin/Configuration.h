f6985ae4850a249973986c969506e123919e9768
Torsten Paul <Torsten.Paul@gmx.de>
2021-04-24 04:48:21 +0200

    Set board to Mini-Rambo (v1.3).

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -134,7 +134,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_RAMPS_14_EFB
+  #define MOTHERBOARD BOARD_MINIRAMBO
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
