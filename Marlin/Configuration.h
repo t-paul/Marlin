ecb3c8d1891ab18dec7428cc8ca135b1ae56924f
Torsten Paul <Torsten.Paul@gmx.de>
2021-04-24 04:50:29 +0200

    Set default axis steps per unit.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -806,7 +806,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 3200/0.8, 350*1.5 }
 
 /**
  * Default Max Feed Rate (mm/s)
